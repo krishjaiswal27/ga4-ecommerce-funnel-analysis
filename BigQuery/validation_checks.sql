80e22199c5b2772295b20a84704fba767168b0b5
Krish Jaiswal <krish271005@gmail.com>
2026-06-20 17:19:52 +0530

    Add files via upload

---
+++ BigQuery/validation_checks.sql
@@ -0,0 +1,24 @@
+SELECT COUNT(*) AS checkout_without_cart
+FROM `project-6a2c2022-16f7-4eef-852.your_dataset.session_funnel`
+WHERE checkout = 1 AND cart = 0;
+
+
+SELECT COUNT(*) AS purchase_without_checkout
+FROM `project-6a2c2022-16f7-4eef-852.your_dataset.session_funnel`
+WHERE purchased = 1 AND checkout = 0;
+
+SELECT
+  COUNTIF(device_category IS NULL) AS null_device,
+  COUNTIF(country IS NULL) AS null_country,
+  COUNTIF(traffic_source IS NULL) AS null_source
+FROM `project-6a2c2022-16f7-4eef-852.your_dataset.session_funnel`;
+
+SELECT
+  COUNT(*) AS sessions,
+  SUM(viewed) AS viewed,
+  SUM(cart) AS cart,
+  SUM(checkout) AS checkout,
+  SUM(purchased) AS purchased,
+  ROUND(100.0 * SUM(viewed) / COUNT(*), 2) AS pct_session_to_view,
+  ROUND(100.0 * SUM(purchased) / COUNT(*), 2) AS pct_overall_conversion
+FROM `project-6a2c2022-16f7-4eef-852.your_dataset.session_funnel`;
